34b223b2f25cc8428de0b5e7c1f1ae127ecf6bdc
phamthaihoang1234 <namtuocbongdem1234567890@gmail.com>
2023-03-28 11:50:40 +0700

    add database

---
+++ src/main/java/com/example/manage_student_lap_ia/ManageStudentLapIaApplication.java
@@ -0,0 +1,13 @@
+package com.example.manage_student_lap_ia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageStudentLapIaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManageStudentLapIaApplication.class, args);
+	}
+
+}
