044b27aebbbd3dbe0a2a36756a51db88e33c5007
phamthaihoang1234 <namtuocbongdem1234567890@gmail.com>
2023-03-30 18:40:10 +0700

    add index

--- src/main/java/com/example/manage_student_lap_ia/controller/StudentController.java
+++ src/main/java/com/example/manage_student_lap_ia/controller/StudentController.java
@@ -1,4 +1,19 @@
 package com.example.manage_student_lap_ia.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+
+@Controller
 public class StudentController {
+    @GetMapping("/")
+    public String showUserList() {
+
+
+        return "index";
+    }
+
 }
